728366d0765453a3ce29f2d70cba7bbed3e6f899
Paulo Montoya <paulo@paulomontoya.com.br>
2020-06-08 18:14:37 -0400

    Add reference links in README and add an example of request

--- src/sagas/example.ts
+++ src/sagas/example.ts
@@ -1,4 +1,5 @@
-import { put, takeLatest, fork } from "redux-saga/effects";
+import { put, takeLatest, fork, call } from "redux-saga/effects";
+import axios from "axios";
 
 import {
   SEND_MESSAGE,
@@ -12,12 +13,15 @@ import {
 function* getList() {
   yield takeLatest(SEND_MESSAGE, function* (action: SendMessageAction) {
     try {
-      console.log("action", action);
       const { payload } = action;
+      console.log("payload", payload);
+
+      const response = yield call(axios, "http://wttr.in/");
+      console.log("response", response);
 
       yield put<SendMessageSuccessAction>({
         type: SEND_MESSAGE_SUCCESS,
-        payload,
+        payload: response.data,
       });
     } catch (e) {
       yield put<SendMessageFailAction>({
