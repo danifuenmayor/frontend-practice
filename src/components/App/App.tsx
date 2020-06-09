384c5e0a244d127d68bf5bbda7ae81a0e20de59f
verolarrondo <vero.larrondo.r@gmail.com>
2020-06-09 10:39:18 -0400

    feat(component): add new routes and add files to reducers
    BREAKING CHANGE: in the files App.txs add routes of the app and comment the example, that break the
    start example with reducers

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,33 +6,44 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../reducers";
 import { SEND_MESSAGE } from "../../reducers/example/types";
 import LoginForm from "../LoginForm";
+import LoginUser from "../LoginUser";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const list = useSelector((state: RootState) => state.example.list);
+  // const dispatch = useDispatch();
+  // const list = useSelector((state: RootState) => state.example.list);
 
-  const handleClick = (event: any) => {
-    dispatch({
-      type: SEND_MESSAGE,
-      payload: {
-        message: "👋",
-      },
-    });
-  };
+  // const handleClick = (event: any) => {
+  //   dispatch({
+  //     type: SEND_MESSAGE,
+  //     payload: {
+  //       message: "👋",
+  //     },
+  //   });
+  // };
 
   return (
     <Router>
       <Switch>
-        <Route path="/login" component={LoginForm}/>
-          <div className={classes.App}>
+        <Route exact path={'/'} component={LoginUser} />
+        <Route exact path={'/login'} component={LoginUser} />
+        {/* <Route exact path={'/register'} component={Register} />
+        <Route exact path={'/login-admin'} component={LoginAdmin} />
+        <Route exact path={'/register-admin'} component={RegisterAdmin} />
+        <Route exact path={'/brands'} component={Brands} />
+        <Route exact path={'/products'} component={Products} />
+        <Route exact path={'/sales-charts'} component={SalesChart} />
+        <Route render={() => <h1>Not found!</h1>} /> */}
+
+
+          {/* <div className={classes.App}>
             <Typography variant="h4" component="h1" className={classes.Title}>
               Welcome to Selyt!
-      </Typography>
+            </Typography>
             <Button variant="contained" color="primary" onClick={handleClick}>
               Hey!
-      </Button>
+          </Button>
             <div className={classes.Emojis}>{list.map(({ message }) => message)}</div>
-          </div>
+          </div> */}
       </Switch>
     </Router>
  );   
---
+++ src/reducers/login/types.ts
@@ -0,0 +1,19 @@
+//example
+export interface Form {
+    fullname: string;
+    mail: string;
+}
+
+export interface StateForm {
+    formUser: Form;
+  }
+
+export const SEND_FORM = "EXAMPLE/SEND_FORM";
+
+export interface SendFormExample {
+  type: typeof SEND_FORM;
+  payload: Form;
+}
+
+export type ExampleSendForm =
+  | SendFormExample
