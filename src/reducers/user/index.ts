a8a727d2e5b747e24cffdfa666c3e7f9836f6ff7
verolarrondo <vero.larrondo.r@gmail.com>
2020-06-16 16:59:06 -0400

    feat(brand): add brand component and its reducers

--- src/reducers/user/index.ts
+++ src/reducers/user/index.ts
@@ -34,7 +34,7 @@ export const UserReducer = (
     case SEND_LOGIN_FAIL:
       return {
         ...state,
-        error: undefined,
+        error: action.payload,
       };
 
     case SEND_LOGIN_SUCCESS:
@@ -55,7 +55,7 @@ export const UserReducer = (
     case SEND_REGISTER_FAIL:
       return {
         ...state,
-        error: undefined,
+        error: action.payload,
       };
 
     case SEND_REGISTER_SUCCESS:
@@ -71,7 +71,7 @@ export const UserReducer = (
     case EDIT_PROFILE_FAIL:
       return {
         ...state,
-        error: undefined,
+        error: action.payload,
       };
 
     case EDIT_PROFILE_SUCCESS:
