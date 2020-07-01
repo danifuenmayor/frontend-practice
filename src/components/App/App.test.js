86c1d6629c39ed01d31f0de806a74ed469425b6e
igcodinap <igcodinap@gmail.com>
2020-07-01 15:06:45 -0400

    prepare to merge with origin dev

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createMockStore } from "redux-test-utils";
+import Enzyme, { mount } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import { MemoryRouter } from "react-router";
+import Home from "../Home/Home";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders home", () => {
+  const wrapper = mount(
+    <Provider store={createMockStore({})}>
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    </Provider>
+  );
+  expect(wrapper.find(Home)).toHaveLength(1);
 });
