769112a4d0a8645ad698f6a079ed79121b35ae3e
Daniela Fuenmayor <danifuenmayor7@gmail.com>
2020-07-09 13:21:11 -0400

    feat(logintest adminlogintest): add tests to login components

--- src/components/LoginUser/LoginForm.test.tsx
+++ src/components/LoginUser/LoginForm.test.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { enzyme, mount } from "../../../enzyme";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import LoginForm from "./LoginForm";
@@ -17,58 +16,66 @@ import {
   sendLoginSuccess,
   sendLoginFail,
 } from "../../reducers/user/actions";
+import { ReactWrapper, mount } from "enzyme";
+import { MemoryRouter } from "react-router-dom";
 
 const mockStore = configureStore();
+let handleSubmit: () => void;
 const store = mockStore({
   user: {
-    accessToken: undefined,
+    name: "JUAN",
+    lastName: "MARTINEZ",
+    email: "test8@example.com",
+    role: "user",
+    accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+    id: "5eea8704983e5e194097155e",
   },
 });
-let container: enzyme.ReactWrapper<
-  Readonly<{}> & Readonly<{ children?: React.ReactNode }>,
-  Readonly<{}>,
-  React.Component<{}, {}, any>
-> | null;
+let container: ReactWrapper;
 
 describe("login form actions", () => {
   const userLogin = {
+    name: "andrea",
+    lastName: "gomez",
     email: "some@email.com",
     password: "somePassword",
+    role: "user",
+    accessToken: "accessToken",
+    id: "4",
   };
   test("should send login action", () => {
     const expectedAction = {
       type: SEND_LOGIN,
       payload: userLogin,
     };
-    expect(sendLogin(userLogin)).toBe(expectedAction);
+    expect(sendLogin(userLogin)).toEqual(expectedAction);
   });
   test("should send success login action", () => {
     const expectedAction = {
       type: SEND_LOGIN_SUCCESS,
       payload: userLogin,
     };
-    expect(sendLoginSuccess(userLogin)).toBe(expectedAction);
+    expect(sendLoginSuccess(userLogin)).toEqual(expectedAction);
   });
   test("should send fail login action", () => {
     const expectedAction = {
       type: SEND_LOGIN_FAIL,
       payload: "someError",
     };
-    expect(sendLoginFail("someError")).toBe(expectedAction);
+    expect(sendLoginFail("someError")).toEqual(expectedAction);
   });
 });
 
 describe("Login Form", () => {
   beforeEach(() => {
+    handleSubmit = jest.fn();
     container = mount(
       <Provider store={store}>
-        <LoginForm />
+        <MemoryRouter initialEntries={["/login"]}>
+          <LoginForm />
+        </MemoryRouter>
       </Provider>
     );
-  });
-
-  afterEach(() => {
-    container = null;
   });
 
   it("should render login form", () => {
@@ -76,10 +83,52 @@ describe("Login Form", () => {
     expect(container.find(TextInput).length).toEqual(2);
     expect(container.find("div.error").length).toEqual(0);
     expect(container.find(Button).length).toEqual(2);
+  });
+});
 
-    // Other example whit state
-    // container.find(".email").simulate("change", {
-    //   target: { name: "email", value: "roddicks@gmail.com" },
-    // });
+test("what happens when the form is not submitted", async () => {
+  const submitButton = container.find('button[type="submit"]');
+  expect(submitButton.props().disabled).toBe(false);
+  expect(handleSubmit).toHaveBeenCalledTimes(0);
+});
+test("should submit data when submit button clicked", async () => {
+  const submitButton = container.find('button[type="submit"]');
+  submitButton.simulate("click");
+  expect(handleSubmit).toHaveBeenCalledTimes(0);
+  container.update();
+  await (() => {
+    expect(handleSubmit).toHaveBeenCalledTimes(1);
+    expect(submitButton.props().disabled).toBe(false);
+    const file = new Blob(["fileContents"], { type: "text/plain" });
+    const emailInput = container.find('input[name="email"]');
+    const passwordInput = container.find('input[name="password"]');
+    emailInput.simulate("change", {
+      target: { name: "email", value: "danif@gmail.com" },
+    });
+    passwordInput.simulate("change", {
+      target: { name: "password", value: "12345" },
+    });
+  });
+  await (() => {
+    const formData = container.find("form");
+    formData.simulate("submit");
+    container.update();
+  });
+  await (() => {
+    expect(handleSubmit).toHaveBeenCalledTimes(1);
+  });
+});
+test("form should not submit if not all inputs are fullfilled", async () => {
+  const submitButton = container.find('button[type="submit"]');
+  submitButton.simulate("click");
+  expect(handleSubmit).toHaveBeenCalledTimes(0);
+  container.update();
+  await (() => {
+    const formData = container.find("form");
+    formData.simulate("submit");
+    container.update();
+  });
+  await (() => {
+    expect(handleSubmit).toHaveBeenCalledTimes(0);
   });
 });
